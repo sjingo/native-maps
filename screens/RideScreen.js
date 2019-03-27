ea47f939c930a928125cecdcb44efb4a73dd69ff
sjingoFOD <43537801+sjingoFOD@users.noreply.github.com>
2019-03-27 08:35:42 +0000

    reverse geolocation on MapScreen

--- screens/RideScreen.js
+++ screens/RideScreen.js
@@ -1,39 +1,148 @@
-import React from 'react';
-import { Dimensions, View } from 'react-native';
-import MapHome from './../components/MapHome';
-import { Input, Text } from 'react-native-elements';
-// import Inputs from './../components/WhereTo'
+import React, {Component} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  PermissionsAndroid,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
 
-const { height, width } = Dimensions.get('window');
+import GetLocation from 'react-native-get-location';
 
-export default class Map extends React.Component {
-  static navigationOptions = {
-    title: 'Map',
-  };
+const {OS} = Platform;
+
+const instructions = Platform.select ({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  location: {
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+    marginBottom: 8,
+  },
+});
+
+export default class App extends Component {
   state = {
-    markers: [{
-      coordinate: {
-        latitude: 53.835927, longitude: -1.776978
-      },
-      title: "Shipley",
-      description: "Center of the universe",
-    }],
-    location: false
-  }
-  setLocation = location => this.setState({ location })
-  render() {
-    const { markers } = this.state;
+    location: null,
+    loading: false,
+  };
+
+  _requestLocation = () => {
+    this.setState ({loading: true, location: null});
+
+    GetLocation.getCurrentPosition ({
+      enableHighAccuracy: true,
+      timeout: 150000,
+    })
+      .then (location => {
+        this.setState ({
+          location,
+          loading: false,
+        });
+      })
+      .catch (ex => {
+        const {code, message} = ex;
+        console.warn (code, message);
+        if (code === 'CANCELLED') {
+          Alert.alert ('Location cancelled by user or by another request');
+        }
+        if (code === 'UNAVAILABLE') {
+          Alert.alert ('Location service is disabled or unavailable');
+        }
+        if (code === 'TIMEOUT') {
+          Alert.alert ('Location request timed out');
+        }
+        if (code === 'UNAUTHORIZED') {
+          Alert.alert ('Authorization denied');
+        }
+        this.setState ({
+          location: null,
+          loading: false,
+        });
+      });
+  };
+
+  render () {
+    const {location, loading} = this.state;
     return (
-      <View style={{ flex: 1, width, height }}>
-        <View style={{ padding: 10, position: 'absolute', top: 50, width, height: 100, flex: 1, zIndex: 1, }}>
-          <Input labelStyle={{ backgroundColor: 'white' }} label="Where to?" raised inputContainerStyle={{ backgroundColor: 'white' }} />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>
+          To get location, press the button:
+        </Text>
+        <View style={styles.button}>
+          <Button
+            disabled={loading}
+            title="Get Location"
+            onPress={this._requestLocation}
+          />
         </View>
-        <View>
-          <Text raised>{this.state.location && this.state.location}</Text>
+        {loading ? <ActivityIndicator /> : null}
+        {location
+          ? <Text style={styles.location}>
+              {JSON.stringify (location, 0, 2)}
+            </Text>
+          : null}
+        <Text style={styles.instructions}>Extra functions:</Text>
+        <View style={styles.button}>
+          <Button
+            title="Open App Settings"
+            onPress={() => {
+              GetLocation.openAppSettings ();
+            }}
+          />
         </View>
-        <MapHome markers={markers} setLocation={this.setLocation} />
+        <View style={styles.button}>
+          <Button
+            title="Open Gps Settings"
+            onPress={() => {
+              GetLocation.openGpsSettings ();
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Open Wifi Settings"
+            onPress={() => {
+              GetLocation.openWifiSettings ();
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Open Mobile Data Settings"
+            onPress={() => {
+              GetLocation.openCelularSettings ();
+            }}
+          />
+        </View>
+        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
-
 }
--- screens/ShareScreen.js
+++ screens/ShareScreen.js
@@ -1,32 +1,50 @@
 import React from 'react';
-import { Dimensions, View } from 'react-native';
+import {Dimensions, View} from 'react-native';
 import MapHome from './../components/MapHome';
-import { Input, Text } from 'react-native-elements';
+import {Input, Text} from 'react-native-elements';
 // import Inputs from './../components/WhereTo'
 
-const { height, width } = Dimensions.get('window');
+const {height, width} = Dimensions.get ('window');
 
 export default class Map extends React.Component {
   static navigationOptions = {
     title: 'Map',
   };
   state = {
-    markers: [{
-      coordinate: {
-        latitude: 53.835927, longitude: -1.776978
+    markers: [
+      {
+        coordinate: {
+          latitude: 53.835927,
+          longitude: -1.776978,
+        },
+        title: 'Shipley',
+        description: 'Center of the universe',
       },
-      title: "Shipley",
-      description: "Center of the universe",
-    }],
-    location: false
-  }
-  setLocation = location => this.setState({ location })
-  render() {
-    const { markers } = this.state;
+    ],
+    location: false,
+  };
+  setLocation = location => this.setState ({location});
+  render () {
+    const {markers} = this.state;
     return (
-      <View style={{ flex: 1, width, height }}>
-        <View style={{ padding: 10, position: 'absolute', top: 50, width, height: 100, flex: 1, zIndex: 1, }}>
-          <Input labelStyle={{ backgroundColor: 'white' }} label="Where to?" raised inputContainerStyle={{ backgroundColor: 'white' }} />
+      <View style={{flex: 1, width, height}}>
+        <View
+          style={{
+            padding: 10,
+            position: 'absolute',
+            top: 50,
+            width,
+            height: 100,
+            flex: 1,
+            zIndex: 1,
+          }}
+        >
+          <Input
+            labelStyle={{backgroundColor: 'white'}}
+            label="Where to?"
+            raised
+            inputContainerStyle={{backgroundColor: 'white'}}
+          />
         </View>
         <View>
           <Text raised>{this.state.location && this.state.location}</Text>
@@ -35,5 +53,4 @@ export default class Map extends React.Component {
       </View>
     );
   }
-
 }
